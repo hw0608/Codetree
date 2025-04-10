3611ca6fa378330547f8c5d352245c4451862cf9
민희원 <hwon.minn@gmail.com>
2025-04-10 16:33:34 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/윤년인가/is-leap-year.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Y;
+    cin>>Y;
+
+    if (Y%4==0 && !(Y%100==0 && Y%400!=0)) {
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+    return 0;
+}
