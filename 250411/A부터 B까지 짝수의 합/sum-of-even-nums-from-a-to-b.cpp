65a95748520624500be2d3ab21e445f12c0df171
민희원 <hwon.minn@gmail.com>
2025-04-11 13:48:49 +0900

    Title: A부터 B까지 짝수의 합, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250411/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin>>A>>B;
+
+    int sum = 0;
+    for (int i=A;i<=B;i++) {
+        if (i % 2 ==0) sum += i;
+    }
+
+    cout << sum;
+    return 0;
+}
